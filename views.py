1a8198fc53ad6180c5d631e26d604e1e85babee1
Charles Cosse <ccosse@gmail.com>
2017-05-07 13:39:11 -0600

    splitting demo account 2x ways -- student or parent

--- views.py
+++ views.py
@@ -13,28 +13,65 @@ FORMAT = 'VIEW: %(message)s'
 
 RLOG_FULL_PATH='/var/www/feeder/creditfeeder/feeder.log'
 logging.basicConfig(filename=RLOG_FULL_PATH,level=logging.DEBUG, format=FORMAT)
+mylogger=logging
+
+def logout_view(request):
+    logging.debug("logout_view")
+    empty_session_id=logout(request)
+    logging.debug("empty_session_id: %s"%empty_session_id)
+    request.session.delete()
+    return HttpResponseRedirect("/")
 
 def home(request):
-    logging.debug("home");
-    if request.user.is_authenticated():
-        logging.debug("already authenticated:"+request.user.username)
-        return app(request)
-    else:
-        ip=request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
-        logging.debug("not authenticated: %s"%(ip))
-        if request.method == 'POST':
-            logging.debug("login POST")
-            user = authenticate(username=request.POST['username'], password=request.POST['password'])
-            if user!=None:
-                logging.debug("logging in user")
-                login(request, user)
-                return app(request)
+	logging.debug("home");
+	if request.user.is_authenticated():
+		logging.debug("already authenticated:"+request.user.username)
+		if request.user.userprofile.is_parent==True:
+			return parent_app(request,request.user.username,{})
+		return student_app(request,request.user.username,{})
 
-        context={'title':'Login','button_text':'Login'}
-        return render(
-                request,'login.html',
-                context
-        )
+	ip=request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
+	logging.debug("not authenticated: %s"%(ip))
+	if request.method == 'POST':
+		mylogger.debug("getting login_pyld from post ...")
+		mylogger.debug(request.POST["login_pyld"])
+		pyld=json.loads(request.POST["login_pyld"])
+		uname=None
+		acct=None
+		try:
+			uname=pyld['device_ip']+"_STUDENT"
+			if pyld['account_type']=="parent":uname=pyld['device_ip']+"_PARENT"
+			acct=User.objects.get(username=uname)
+			mylogger.debug("got user")
+			login(request,acct)
+			mylogger.debug("logged-in user "+uname)
+
+		except:
+			mylogger.debug("creating new account ...")
+			uname=pyld['device_ip']+"_STUDENT"
+			if pyld["account_type"]=="parent":uname=pyld['device_ip']+"_PARENT"
+			acct=User.objects.create_user(username=uname,password='pycon2017')
+			acct.userprofile.is_parent=False
+			if pyld["account_type"]=="parent":acct.userprofile.is_parent=True
+			acct.userprofile.save()
+			acct.save()
+
+			mylogger.debug("logging-in "+uname)
+			login(request,acct)
+
+		if acct.userprofile.is_parent==True:
+			return parent_app(request,uname,pyld)
+
+		return student_app(request,uname,pyld)
+
+	context={
+		'title':'Credit Feeder Login',
+		'device_ip':ip,
+	}
+	return render(
+		request,'device-login.html',
+		context
+	)
 
 def get(request):
 	progress=0
@@ -58,12 +95,20 @@ def get(request):
 
 
 @login_required
-def app(request):
-        context={
-                'title':'CreditFeeder',
-                'username':request.user.username,
-                'is_parent':request.user.userprofile.is_parent,
-                'credit_balance':request.user.userprofile.credit_balance,
-        }
-        return render(request,'creditfeeder.html',context)
+def student_app(request,username,pyld):
+	context={
+		'title':'Student@CreditFeeder',
+		'username':username,
+		'credit_balance':request.user.userprofile.credit_balance,
+		'json_pyld':json.dumps(pyld),
+	}
+	return render(request,'student_app.html',context)
 
+@login_required
+def parent_app(request,username,pyld):
+	context={
+		'title':'Parent@CreditFeeder',
+		'username':username,
+		'json_pyld':json.dumps(pyld),
+	}
+	return render(request,'parent_app.html',context)
